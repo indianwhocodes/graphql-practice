61e353f93ba4acab48836e4f3444549be134888f
Ashwin A Nair <ashwin.nair@joyent.com>
2021-03-02 15:39:56 -0800

    Schema restructuring

---
+++ index.js
@@ -0,0 +1,13 @@
+import mySchema from "./schema/mySchema.js";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
+app.use(`/graphql`, graphqlHTTP({
+        schema: mySchema,
+        graphiql: true
+}));
+
+app.listen(4500, () => {
+    console.log('Server is running on port 3500')
+});
---
+++ schema/mySchema.js
@@ -0,0 +1,85 @@
+import {
+    GraphQLInt,
+    GraphQLList,
+    GraphQLString,
+    GraphQLObjectType,
+    GraphQLSchema,
+} from "graphql";
+import {
+    fromGlobalId,
+    globalIdField,
+    nodeDefinitions,
+} from 'graphql-relay';
+
+import fetch from 'node-fetch';
+
+const BASE_URL = 'http://localhost:3600'
+
+function fetchResponseByURL(relativeURL) {
+    return fetch(`${BASE_URL}${relativeURL}`).then(res => res.json());
+}
+
+function fetchPeople() {
+    return fetchResponseByURL('/users/');
+}
+
+function fetchPersonByURL(relativeURL) {
+    return fetchResponseByURL(relativeURL).then(json => json._id);
+}
+
+const { nodeInterface, nodeField } = nodeDefinitions(
+    globalId => {
+        const { type, id } = fromGlobalId(globalId);
+        if (type === 'User') {
+            return fetchPersonByURL(`/users/${id}/`);
+        }
+    },
+    object => {
+        if (object.hasOwnProperty('username')) {
+            return 'User';
+        }
+    },
+)
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    description: 'A Person stored in the database',
+    fields: () => ({
+        firstName: {
+            type: GraphQLString,
+            resolve: person => person.first_name,
+        },
+        lastName: {
+            type: GraphQLString,
+            resolve: person => person.last_name,
+        },
+        email: {type: GraphQLString},
+        id: globalIdField('Person'),
+    }),
+    interfaces: [ nodeInterface ],
+});
+
+const QueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Source of all queries',
+
+    fields: () => ({
+        allPeople: {
+            type: new GraphQLList(UserType),
+            resolve: fetchPeople,
+       },
+        node: nodeField,
+        user: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLString },
+            },
+            resolve: (root, args) =>
+                    fetchPersonByURL(`/users/${args.id}/`),
+            },
+    }),
+});
+
+export default new GraphQLSchema({
+    query: QueryType,
+})
